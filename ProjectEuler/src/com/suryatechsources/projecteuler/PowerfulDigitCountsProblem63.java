c2043be4738b50ecf76a42a876cef13d80d8ae29
suryadavinci <suryadavinci@gmail.com>
2016-12-09 16:24:25 +0530

    Completed Problem63

--- ProjectEuler/src/com/suryatechsources/projecteuler/PowerfulDigitCountsProblem63.java
+++ ProjectEuler/src/com/suryatechsources/projecteuler/PowerfulDigitCountsProblem63.java
@@ -2,9 +2,54 @@ package com.suryatechsources.projecteuler;
 
 public class PowerfulDigitCountsProblem63 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		long x=0,count=0;
+		
+		
+		
+		double n=3;
+		double a=5;
+		
+		System.out.println(n*Math.log10(a));
+		
+		double a1=1;
+		for(n=1;;n++)
+		{	
+		//	a=a1;
+			for(a=1;a<=9;a++)
+			{
+				if(n*Math.log10(a) >= (n-1) & n*Math.log10(a)<n)
+					{count++;
+					//System.out.println("Success : "+a+" "+n);
+					System.out.println(count+" "+a+" "+n+" "+n*Math.log10(a)+" "+n*Math.log10(a));
+					}
+				if(n*Math.log10(a) > n){	
+					//count++;
+				System.out.println(count+" fail "+a+" "+n+" "+n*Math.log10(a)+" "+n*Math.log10(a));
+				break;
+				}
+				
+				
+				
+				
+			}
+			
+		
+		}
+		
+		
+		
 
+	}
+	
+	
+	public static int digitCount(long x)
+	{
+		return Long.toString(x).length();
 	}
 
 }
