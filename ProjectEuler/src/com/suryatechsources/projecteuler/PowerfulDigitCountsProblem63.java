13d075c88279f9cdf433fdeb976406f056988842
suryadavinci <suryadavinci@gmail.com>
2016-12-09 14:24:24 +0530

    newly added

---
+++ ProjectEuler/src/com/suryatechsources/projecteuler/PowerfulDigitCountsProblem63.java
@@ -0,0 +1,10 @@
+package com.suryatechsources.projecteuler;
+
+public class PowerfulDigitCountsProblem63 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
