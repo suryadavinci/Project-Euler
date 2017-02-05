472ab0362bd5476d49608e30f7e6627b5ae7e7c0
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-06 00:35:12 +0530

    Commiting as suryadavinci@gmail.com

---
+++ ProjectEuler/src/com/suryatechsources/projecteuler/LongTimeNoSee.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.suryatechsources.projecteuler;
+
+/**
+ * @author surya
+ *
+ */
+public class LongTimeNoSee {
+
+	/**
+	 * 
+	 */
+	public LongTimeNoSee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
