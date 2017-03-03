a0a8edfee84c07324aa3f4cb6b8cbbd87d79f92f
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-03 16:20:01 +0530

    Done

---
+++ ProjectEuler/src/com/suryatechsources/projecteuler/Problem059XORDecryption.java
@@ -0,0 +1,49 @@
+package com.suryatechsources.projecteuler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Problem059XORDecryption {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		File file= new File("p059_cipher.txt");
+			
+		Scanner s= new Scanner(file);
+		
+		String x = s.next();
+		
+		List<String> l=  Arrays.asList(x.split(","));
+		
+		
+		int[] array= new int[1201];
+		
+		for(int i=0;i<array.length;i++)
+		{
+			array[i]=Integer.parseInt(l.get(i));
+		}
+		System.out.println(array[1200]);
+		
+		
+		for(int i=97;i<=122;i++){
+			
+			int[] a= new int[1201];
+			
+			
+			for(int j=0;j<1201;j++){
+				a[j]=array[j]^i;
+				System.out.print((char)a[j] );
+			}
+			System.out.println();
+			
+			
+		}
+		
+		
+		
+	}
+
+}
