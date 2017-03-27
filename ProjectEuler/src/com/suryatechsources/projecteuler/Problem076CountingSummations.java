b7badaf2fa54cfac6f82fbc8bf1824e76f46c615
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-27 18:59:03 +0530

    Works like a charm

--- ProjectEuler/src/com/suryatechsources/projecteuler/Problem076CountingSummations.java
+++ ProjectEuler/src/com/suryatechsources/projecteuler/Problem076CountingSummations.java
@@ -1,43 +1,51 @@
 package com.suryatechsources.projecteuler;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 
 public class Problem076CountingSummations {
 	static int count;
-	static HashMap<String,Long> mem= new HashMap<String,Long>();
+	static HashMap<String, Long> mem = new HashMap<String, Long>();
+	static HashMap<String, BigInteger> memBig = new HashMap<String, BigInteger>();
+
 	public static void main(String[] args) {
-		long start=System.currentTimeMillis();
-		int i=56;
-		long output=0;
-		while(true)
-			{
-			System.out.println(output=countSum(i,0));
-			if(output%10000000==0)
-				{
-					System.out.println(i);
-					break;
-				}
-			i++;
-			
-			}
-			
-			System.out.println("Time Taken : "+(System.currentTimeMillis()-start));
+		long start = System.currentTimeMillis();
+		int i = 1;
+		long output;
+		System.out.println(100 + " " + (output = countSum(100, 0)));
+		System.out.println("Time Taken : "
+				+ (System.currentTimeMillis() - start));
 	}
-	public static long countSum(int sum, int minimal){
-		long temp=1;
-		if(sum==1)
-			{
-				mem.put(sum+"-"+minimal, 1l);
-				return 1;
-			}
-		
-		if(mem.containsKey(sum+"-"+minimal))return mem.get(sum+"-"+minimal);
-		
-		for(int i=1;i<=sum/2;i++){
-			if(i>=minimal)
-				temp=temp+ countSum(sum-i,i);
+
+	public static long countSum(int sum, int minimal) {
+		long temp = 1;
+		if (sum == 1) {
+			mem.put(sum + "-" + minimal, 1l);
+			return 1;
 		}
-		mem.put(sum+"-"+minimal, temp);
+		if (mem.containsKey(sum + "-" + minimal))
+			return mem.get(sum + "-" + minimal);
+		for (int i = 1; i <= sum / 2; i++) {
+			if (i >= minimal)
+				temp = temp + countSum(sum - i, i);
+		}
+		mem.put(sum + "-" + minimal, temp);
+		return temp;
+	}
+
+	public static BigInteger countSumBig(int sum, int minimal) {
+		BigInteger temp = BigInteger.ONE;
+		if (sum == 1) {
+			memBig.put(sum + "-" + minimal, BigInteger.ONE);
+			return BigInteger.ONE;
+		}
+		if (mem.containsKey(sum + "-" + minimal))
+			return memBig.get(sum + "-" + minimal);
+		for (int i = 1; i <= sum / 2; i++) {
+			if (i >= minimal)
+				temp = temp.add(countSumBig(sum - i, i));
+		}
+		memBig.put(sum + "-" + minimal, temp);
 		return temp;
 	}
 }
