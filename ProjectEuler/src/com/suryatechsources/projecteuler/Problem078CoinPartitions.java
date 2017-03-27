649663de2e92043a13f59cb5e5c1f201911b3366
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-27 18:59:19 +0530

    Needs Tweeking

---
+++ ProjectEuler/src/com/suryatechsources/projecteuler/Problem078CoinPartitions.java
@@ -0,0 +1,30 @@
+package com.suryatechsources.projecteuler;
+
+import java.math.BigInteger;
+
+public class Problem078CoinPartitions {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		int i = 1;
+		BigInteger output;
+		BigInteger div = new BigInteger("1000000");
+		while (true) {
+			System.out
+					.println(i
+							+ " "
+							+ (output = Problem076CountingSummations
+									.countSumBig(i, 0)));
+			if (output.mod(div) == BigInteger.ZERO) {
+				System.out.println(i);
+				break;
+			}
+			i++;
+
+		}
+
+		System.out.println("Time Taken : "
+				+ (System.currentTimeMillis() - start));
+	}
+
+}
