073af1f9e8c5ab5577cb7b1d297ab7d5eaa3ca55
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-27 18:42:48 +0530

    done

--- ProjectEuler/src/com/suryatechsources/projecteuler/Problem076CountingSummations.java
+++ ProjectEuler/src/com/suryatechsources/projecteuler/Problem076CountingSummations.java
@@ -4,27 +4,40 @@ import java.util.HashMap;
 
 public class Problem076CountingSummations {
 	static int count;
-	static HashMap<String,Integer> mem= new HashMap<String,Integer>();
+	static HashMap<String,Long> mem= new HashMap<String,Long>();
 	public static void main(String[] args) {
-		System.out.println(countSum(5,0));
-		
-	}
-	
-	public static int countSum(int eSum, int current){
-		int val;
-		if(mem.containsKey(eSum+"-"+current))
-			return mem.get(eSum+"-"+current);
-		
-		
-		for(int i=1;i<=eSum/current;i++){
-			val=eSum-i*current;
-			if(val==0)
+		long start=System.currentTimeMillis();
+		int i=56;
+		long output=0;
+		while(true)
+			{
+			System.out.println(output=countSum(i,0));
+			if(output%10000000==0)
 				{
-				mem.put(eSum+"-"+current,1);
+					System.out.println(i);
+					break;
 				}
-			else if(val>0)
-				count=1+countSum(val,count);
+			i++;
+			
+			}
+			
+			System.out.println("Time Taken : "+(System.currentTimeMillis()-start));
+	}
+	public static long countSum(int sum, int minimal){
+		long temp=1;
+		if(sum==1)
+			{
+				mem.put(sum+"-"+minimal, 1l);
+				return 1;
+			}
+		
+		if(mem.containsKey(sum+"-"+minimal))return mem.get(sum+"-"+minimal);
+		
+		for(int i=1;i<=sum/2;i++){
+			if(i>=minimal)
+				temp=temp+ countSum(sum-i,i);
 		}
-		return count;
+		mem.put(sum+"-"+minimal, temp);
+		return temp;
 	}
 }
